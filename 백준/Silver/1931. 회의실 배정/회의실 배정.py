8a9bffd4133224e612ce8a3620b56565d7dafe0a
eb9862 <72558397+eb9862@users.noreply.github.com>
2024-04-29 17:15:19 +0900

    [Silver I] Title: 회의실 배정, Time: 432 ms, Memory: 125688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+lst = [[] for _ in range(N)]
+for i in range(N):
+    lst[i] = list(map(int, input().rstrip().split()))
+
+lst.sort()
+lst.sort(key=lambda x: x[1])
+
+cnt = 0
+end = 0
+for t in lst:
+    if t[0] >= end:
+        end = t[1]
+        cnt += 1
+print(cnt)
