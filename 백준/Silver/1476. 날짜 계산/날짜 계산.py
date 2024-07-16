174a6b9da94e356a99b3b740bb9ad53f678a4f95
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2024-07-16 10:40:49 +0900

    [Silver V] Title: 날짜 계산, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,15 @@
+E, S, M = map(int, input().split())
+e, s, m = 1, 1, 1
+y = 1
+while not(e == E and s == S and m == M):
+    e += 1
+    s += 1
+    m += 1
+    if e == 16:
+        e = 1
+    if s == 29:
+        s = 1
+    if m == 20:
+        m = 1
+    y += 1
+print(y)
