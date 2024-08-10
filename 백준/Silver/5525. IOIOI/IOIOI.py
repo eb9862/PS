9d5ccaafaf5ca5c4aa092f49b352a7c77f38ee6a
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2024-08-10 23:47:20 +0900

    [Silver I] Title: IOIOI, Time: 112 ms, Memory: 113124 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,10 +2,19 @@ N = int(input())
 M = int(input())
 s = input()
 
-target = 'O'.join((N+1) * 'I')
-l = N*2 + 1
 cnt = 0
-for i in range(M-l+1):
-    if s[i] == 'I' and s[i:i+l] == target:
-        cnt += 1
+i = 0
+last = M-2
+n = 0
+while i < last:
+    if s[i:i+3] == "IOI":
+        n += 1
+        i += 2
+        if n == N:
+            cnt += 1
+            n -= 1
+    else:
+        n = 0
+        i += 1
+
 print(cnt)
