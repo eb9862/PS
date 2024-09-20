23a354f6b33c20a6bfe49e67bb854f84f14657d1
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2024-09-20 19:32:06 +0900

    [Bronze I] Title: 최소공배수, Time: 520 ms, Memory: 23720 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -3,32 +3,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int n = scanner.nextInt();
+        int n = sc.nextInt();
+
         for (int i = 0; i < n; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int gcd = calGcd(a, b);
-            int lcm = gcd * (a / gcd) * (b / gcd);
-            System.out.println(lcm);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int l = ba(a, b);
+            int c = l;
+            int h = ab(a, b);
+            
+            while (l % h != 0) {
+                l += c;
+            }
+            System.out.println(l);
         }
     }
 
-    public static int calGcd(int a, int b) {
-        int remain;
-        if (a > b) {
-            remain = a % b;
-            if (remain == 0) {
-                return b;
-            }
-            return calGcd(b, remain);
-        } else {
-            remain = b % a;
-            if (remain == 0) {
-                return a;
-            }
-            return calGcd(a, remain);
-        }
+    public static int ab(int a, int b) {
+
+        if (a >= b)
+            return a;
+        return b;
+    }
+
+    public static int ba(int a, int b) {
+
+        if (a > b)
+            return b;
+        return a;
     }
 }
