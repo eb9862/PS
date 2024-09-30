4c95a00e1f4f8b196dba0186a542fc2f6bb8a94e
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2024-09-30 15:51:01 +0900

    [Silver I] Title: 인형들, Time: 192 ms, Memory: 14736 KB -BaekjoonHub

---
+++ 백준/Silver/15954. 인형들/인형들.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dolls;
+    static int n, k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        dolls = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            dolls[i] = Integer.parseInt(st.nextToken());
+        }
+
+        double result = Double.MAX_VALUE;
+        for (int j = k; j < n + 1; j++) {
+            for (int i = 0; i < n - j + 1; i++) {
+                double variance = calVar(i, j);
+                if (variance < result) {
+                    result = variance;
+                }
+            }
+        }
+        double stdDeviation = Math.sqrt(result);
+        System.out.println(stdDeviation);
+    }
+
+    static double calVar(int idx, int cnt) {
+        double sumValue = 0;
+        double avg = calAvg(idx, cnt);
+        for (int i = idx; i < idx + cnt; i++) {
+            double sub = dolls[i] - avg;
+            sumValue += Math.pow(sub, 2);
+        }
+        return sumValue / cnt;
+    }
+
+    static double calAvg(int idx, int cnt) {
+        double sumValue = 0;
+        for (int i = idx; i < idx + cnt; i++) {
+            sumValue += dolls[i];
+        }
+        return sumValue / cnt;
+    }
+}
