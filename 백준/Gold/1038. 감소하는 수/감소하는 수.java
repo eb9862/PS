7c4fed6e65ecc5a814ad27282a32afed97149ee7
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2024-10-14 15:01:18 +0900

    [Gold V] Title: 감소하는 수, Time: 132 ms, Memory: 16232 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n, result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        n = Integer.parseInt(br.readLine());
+
+        if (n < 10) {
+            System.out.println(n);
+            System.exit(0);
+        }
+
+        result = 9;
+        for (int l = 2; l <= 10; l++) {
+            long num = 0;
+            bt(num, 0, l);
+        }
+        System.out.println(-1);
+    }
+
+    static void bt(long num, int nowLen, int l) {
+        if (nowLen == l) {
+            result++;
+            //System.out.println(result + "th : " + num);
+            if (result == n) {
+                System.out.println(num);
+                System.exit(0);
+            }
+            return;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (nowLen == 0 && i != 0) {
+                bt(num + i, nowLen + 1, l);
+            }
+            if (num % 10 > i) {
+                bt(num * 10 + i, nowLen + 1, l);
+            }
+        }
+    }
+}
