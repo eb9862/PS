06e85f5ec87ee6ace09208ca1b15d299d6f90f1e
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2024-12-07 16:26:57 +0900

    Delete goormlevel directory

--- goormlevel/47882/1. 헷갈리는 작대기/헷갈리는 작대기.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# UTF-8 encoding when using korean
-user_input = input()
-print(user_input.count('1'))
-print(user_input.count('I'))
-print(user_input.count('l'))
-print(user_input.count('|'))
