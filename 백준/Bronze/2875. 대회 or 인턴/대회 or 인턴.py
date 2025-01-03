09f89da5dae520d7c586ce0f51295b9e85e71fc3
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2025-01-03 14:55:28 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -1,16 +1,8 @@
 n, m, k = map(int, input().split())
 
-if n + m == k:
-    print(0)
-    exit()
-
 teams = min(n // 2, m)
 n -= teams * 2
 m -= teams
-
-if n + m >= k:
-    print(teams)
-    exit()
 
 k -= n + m
 while k > 0:
