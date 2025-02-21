6ca03978718fe1f152c7200212f80c75ba13e89d
Eunbae Hwang <72558397+eb9862@users.noreply.github.com>
2025-02-21 21:10:06 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 458.47 ms, Memory: 14 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+def can_move(y: int, x: int, row: int, col: int) -> bool:
+    return (0 <= y and y < row) and (0 <= x and x < col)
+
+def find_points_to_move(y: int, x: int, row: int, col: int) -> list:
+    
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    
+    points = []
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if can_move(ny, nx, row, col):
+            points.append((ny, nx))
+    return points
+
+def bfs(land: list, y: int, x: int, visited: int):
+    
+    row = len(land)
+    col = len(land[0])
+    
+    visited[y][x] = True
+    q = deque([(y, x)])
+    
+    result = 0
+    cols = set()
+    while q:
+        y, x = q.popleft()
+        cols.add(x)
+        result += 1
+        
+        points = find_points_to_move(y, x, row, col)
+        
+        for y, x in points:
+            if not visited[y][x]:
+                visited[y][x] = True
+                if land[y][x] == 1:
+                    q.append((y, x))
+    return cols, result                               
+
+def solution(land):
+    answer = 0
+    
+    row = len(land)
+    col = len(land[0])
+    
+    oils = [ 0 for _ in range(col)]
+    visited = [ [False for _ in range(col)] for _ in range(row)]
+    for y in range(row):
+        for x in range(col):
+            if land[y][x] == 1 and not visited[y][x]:
+                cols, quantity = bfs(land, y, x, visited)
+                for c in cols:
+                    oils[c] += quantity
+    
+    return max(oils)
